a213fe0ddd69ef65e86d7356d2c9cea56593c14f
emanuelhm <miyagawa.emanuel@gmail.com>
2019-11-18 16:37:53 -0300

    Initial Commit

---
+++ UnityEasyARCustomization/ImageTarget.cs
@@ -0,0 +1,106 @@
+﻿using easyar;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace UnityEasyARCustomization
+{
+    public class ImageTarget : ImageTargetController
+    {
+        public UnityEvent OnActive = null;
+
+        public Sprite Sprite = null;
+
+        private byte[] _imageData;
+
+        public void Initialize()
+        {
+            _imageData = Sprite.texture.EncodeToPNG();
+
+            switch (targetType)
+            {
+                case TargetType.LocalImage:
+                case TargetType.LocalTargetData:
+                    if (gameObject.activeSelf)
+                        StartCoroutine(LoadImageTarget());
+                    break;
+                case TargetType.Cloud:
+                    if (target != null)
+                    {
+                        TargetName = target.name();
+                    }
+                    break;
+            }
+        }
+
+        public override void Start() { }
+
+        private IEnumerator LoadImageTarget()
+        {
+            var path = TargetPath;
+            var type = Type;
+
+            var data = _imageData;
+            Buffer buffer = Buffer.create(data.Length);
+            var ptr = buffer.data();
+            System.Runtime.InteropServices.Marshal.Copy(data, 0, ptr, data.Length);
+
+            Optional<easyar.ImageTarget> op_target;
+            if (targetType == TargetType.LocalImage)
+            {
+                var image = ImageHelper.decode(buffer);
+                if (!image.OnSome)
+                {
+                    throw new System.Exception("decode image file data failed");
+                }
+
+                var p = new ImageTargetParameters();
+                p.setImage(image.Value);
+                p.setName(TargetName);
+                p.setScale(TargetSize);
+                p.setUid("");
+                p.setMeta("");
+                op_target = easyar.ImageTarget.createFromParameters(p);
+
+                if (!op_target.OnSome)
+                {
+                    throw new System.Exception("create image target failed from image target parameters");
+                }
+
+                image.Value.Dispose();
+                buffer.Dispose();
+                p.Dispose();
+            }
+            else
+            {
+                op_target = easyar.ImageTarget.createFromTargetData(buffer);
+
+                if (!op_target.OnSome)
+                {
+                    throw new System.Exception("create image target failed from image target target data");
+                }
+
+                buffer.Dispose();
+            }
+
+            target = op_target.Value;
+            if (ImageTracker == null)
+            {
+                yield break;
+            }
+            ImageTracker.LoadImageTarget(this, (_target, status) =>
+            {
+                targetImage = ((_target as easyar.ImageTarget).images())[0];
+                Debug.Log("[EasyAR] Targtet name: " + _target.name() + " target runtimeID: " + _target.runtimeID() + " load status: " + status);
+                Debug.Log("[EasyAR] Target size" + targetImage.width() + " " + targetImage.height());
+                OnActive.Invoke();
+                gameObject.SetActive(false);
+            });
+        }
+
+        public override void OnTracking(Matrix4x4 pose)
+        {
+            transform.localScale = transform.localScale * TargetSize;
+        }
+    }
+}
---
+++ UnityEasyARCustomization/ImageTrackerBehaviour.cs
@@ -0,0 +1,110 @@
+﻿using easyar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace UnityEasyARCustomization
+{
+    class ImageTrackerBehaviour : easyar.ImageTrackerBehaviour
+    {
+        public override void UpdateFrame(ARSessionUpdateEventArgs args)
+        {
+            List<ImageTargetController> currentTrackingControllers = new List<ImageTargetController>();
+            var results = args.OFrame.results();
+
+            foreach (var _result in results)
+            {
+                ImageTrackerResult result = null;
+                if (_result.OnSome)
+                {
+                    result = _result.Value as ImageTrackerResult;
+                }
+
+                if (result != null)
+                {
+                    var targetInstances = result.targetInstances();
+                    int centerTargetId = -1;
+                    if (TargetCamera == null)
+                    {
+                        Utility.SetMatrixOnTransform(Camera.main.transform, centerTransform);
+                    }
+                    else
+                    {
+                        Utility.SetMatrixOnTransform(TargetCamera.transform, centerTransform);
+                    }
+                    if (CenterImageTarget != null && CenterImageTarget.Target() != null && CenterTarget == CenterMode.SpecificTarget)
+                    {
+                        centerTargetId = CenterImageTarget.Target().runtimeID();
+                    }
+                    foreach (var targetInstance in targetInstances)
+                    {
+                        var target = targetInstance.target();
+                        if (!target.OnSome)
+                        {
+                            continue;
+                        }
+                        var status = targetInstance.status();
+                        foreach (var targetController in targetControllers)
+                        {
+                            var _target = targetController.Target();
+                            if (target.Value.runtimeID() == _target.runtimeID())
+                            {
+                                if (status == TargetStatus.Tracked)
+                                {
+                                    if (!targetController.Tracked)
+                                    {
+                                        targetController.OnFound();
+                                        targetController.Tracked = true;
+                                    }
+                                    var pose = Utility.Matrix44FToMatrix4x4(targetInstance.pose());
+
+                                    pose = args.ImageRotationMatrixGlobal * pose * Matrix4x4.Rotate(Quaternion.Euler(-90, 0, 180));
+
+                                    if (CenterTarget == CenterMode.FirstTarget && centerTargetId == -1)
+                                    {
+                                        centerTargetId = target.Value.runtimeID();
+                                        CenterImageTarget = targetController;
+                                    }
+
+                                    if (centerTargetId != target.Value.runtimeID())
+                                    {
+                                        pose = centerTransform * pose;
+                                        targetController.OnTracking(pose);
+                                    }
+                                    else
+                                    {
+                                        targetController.OnTracking(Matrix4x4.identity);
+                                        centerTransform = pose.inverse;
+                                    }
+                                    currentTrackingControllers.Add(targetController);
+                                }
+                            }
+                        }
+                        target.Value.Dispose();
+                        targetInstance.Dispose();
+                    }
+                    result.Dispose();
+                }
+            }
+            foreach (var targetController in targetControllers)
+            {
+                bool contain = false;
+                foreach (var item in currentTrackingControllers)
+                {
+                    if (item == targetController)
+                    {
+                        contain = true;
+                    }
+                }
+                if (!contain && targetController.Tracked)
+                {
+                    targetController.OnLost();
+                    targetController.Tracked = false;
+                }
+            }
+        }
+    }
+}
